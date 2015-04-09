85b7f8a32a0c74e19325ace62d88a9bcca8a2d9e
Ali Anari <ali@anari.me>
2015-04-09 13:43:14 -0400

    Auth fixes

--- lib/WebService/SiftScience.pm
+++ lib/WebService/SiftScience.pm
@@ -10,9 +10,11 @@ has '+base_url' => ( default => 'http://api.siftscience.com/v203' );
 has api_key     => ( is => 'ro', required => 1                    );
 has events_uri  => ( is => 'ro', default => '/events'             );
 has score_uri   => ( is => 'ro', default => '/score'              );
+has users_uri   => ( is => 'ro', default => '/users'              );
 
 method get_score (Str $user_id) {
-    return $self->get($self->score_uri);
+    return $self->get($self->_score_uri($user_id) .
+        '?api_key=' . $self->api_key);
 }
 
 method create_event (Str $user_id, Str $type, Maybe[HashRef] $data = {}) {
@@ -60,7 +62,7 @@ method send_message (Str $user_id, Maybe[HashRef] $data) {
     return $self->create_event($user_id, '$send_message', $data);
 }
 
-method login (Str $user_id, Maybe[HashRef] $data) {
+method login (Str $user_id, Maybe[HashRef] $data = {}) {
     return $self->create_event($user_id, '$login', $data);
 }
 
@@ -70,6 +72,26 @@ method logout (Str $user_id) {
 
 method custom_event (Str $user_id, Str $type, Maybe[HashRef] $data) {
     return $self->create_event($user_id, $type, $data);
+}
+
+method label_user (Str $user_id, $data) {
+    return $self->post($self->_label_uri($user_id), {
+        '$api_key'  => $self->api_key,
+        ( %$data ) x!! $data,
+    });
+}
+
+method unlabel_user (Str $user_id) {
+    return $self->delete($self->_label_uri($user_id) .
+        '?api_key=' . $self->api_key);
+}
+
+method _score_uri (Str $user_id) {
+    return $self->score_uri . "/$user_id";
+}
+
+method _label_uri (Str $user_id) {
+    return $self->users_uri . "/$user_id/labels";
 }
 
 1;
